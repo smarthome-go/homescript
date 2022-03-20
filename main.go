e63539ffee794f47f3779dcfc435fb48e83f8777
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-03-20 15:58:29 +0100

    fix `main.go`

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/MikMuellerDev/homescript-dev/homescript"
+import "github.com/MikMuellerDev/homescript/homescript"
 
 func main() {
 	_, err := homescript.Run(homescript.DummyExecutor{}, "print('hello world')")
