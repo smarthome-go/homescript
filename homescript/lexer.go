176eefc57e0923bc65e7906087d3bae51598dcbc
RubixDev <silas.groh@t-online.de>
2022-03-21 16:19:08 +0100

    Put util functions in `lexer.go`

--- homescript/lexer.go
+++ homescript/lexer.go
@@ -6,6 +6,16 @@ import (
 	"github.com/MikMuellerDev/homescript/homescript/error"
 )
 
+func isDigit(char rune) bool {
+	intChar := int(char)
+	return intChar >= 48 && intChar <= 57
+}
+
+func isLetter(char rune) bool {
+	intChar := int(char)
+	return intChar >= 65 && intChar <= 90 || intChar >= 97 && intChar <= 122
+}
+
 type Lexer struct {
 	CurrentChar  *rune
 	CurrentIndex uint
