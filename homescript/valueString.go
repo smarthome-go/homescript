35b43d9cfe0384c682d8afede3790b25380b1f76
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-10-27 17:19:35 +0200

    Add JSON unmarshal support

--- homescript/valueString.go
+++ homescript/valueString.go
@@ -1,6 +1,7 @@
 package homescript
 
 import (
+	"encoding/json"
 	"fmt"
 	"strings"
 	"unicode/utf8"
@@ -61,8 +62,77 @@ func (self ValueString) Fields() map[string]*Value {
 				}, nil, nil
 			},
 		}),
+		"parse_json": valPtr(ValueBuiltinFunction{
+			Callback: func(executor Executor, span errors.Span, args ...Value) (Value, *int, *errors.Error) {
+				var raw interface{}
+				if err := json.Unmarshal([]byte(self.Value), &raw); err != nil {
+					return nil, nil, errors.NewError(span, err.Error(), errors.ValueError)
+				}
+				value, err := unmarshalValue(span, raw)
+				if err != nil {
+					return nil, nil, err
+				}
+				return value, nil, nil
+			},
+		}),
 	}
 }
+
+func unmarshalValue(span errors.Span, self interface{}) (Value, *errors.Error) {
+	switch self := self.(type) {
+	case string:
+		return ValueString{Value: self}, nil
+	case float64:
+		return ValueNumber{Value: self}, nil
+	case int:
+		return ValueNumber{Value: float64(self)}, nil
+	case bool:
+		return ValueBool{Value: self}, nil
+	case map[string]interface{}:
+		output := ValueObject{ObjFields: make(map[string]*Value)}
+		for key, field := range self {
+			value, err := unmarshalValue(span, field)
+			if err != nil {
+				return nil, err
+			}
+			output.ObjFields[key] = &value
+		}
+		return output, nil
+	case []interface{}:
+		values := make([]*Value, 0)
+		valueType := TypeUnknown
+		for _, item := range self {
+			value, err := unmarshalValue(span, item)
+			if err != nil {
+				return nil, err
+			}
+			// Check type equality
+			if valueType != TypeUnknown {
+				if valueType != value.Type() {
+					return nil, errors.NewError(
+						span,
+						fmt.Sprintf("type inconsistency in list: cannot insert value of type '%v' into %v<%v>", value.Type(), TypeList, valueType),
+						errors.RuntimeError,
+					)
+				}
+			} else {
+				valueType = value.Type()
+			}
+			values = append(values, &value)
+		}
+		return ValueList{
+			Values:    &values,
+			ValueType: &valueType,
+		}, nil
+	default:
+		return nil, errors.NewError(
+			span,
+			fmt.Sprintf("cannot parse unknown JSON to HMS value: %v", self),
+			errors.RuntimeError,
+		)
+	}
+}
+
 func (self ValueString) Index(_ Executor, index int, span errors.Span) (*Value, *errors.Error) {
 	// Check the string len
 	valLen := len(self.Value)
