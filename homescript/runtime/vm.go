cb4b1d7373a29cb4ccde97b02d8604a427ea4f8d
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2024-02-07 02:25:22 +0100

    feat: VM func API: type validation and ret. values

--- homescript/runtime/vm.go
+++ homescript/runtime/vm.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"sync"
 
+	"github.com/smarthome-go/homescript/v3/homescript/analyzer/ast"
 	"github.com/smarthome-go/homescript/v3/homescript/compiler"
+	"github.com/smarthome-go/homescript/v3/homescript/errors"
 	"github.com/smarthome-go/homescript/v3/homescript/runtime/value"
 )
 
@@ -113,43 +115,155 @@ func (self *VM) spawnCore() *Core {
 	return &core
 }
 
+type FunctionInvocationSignature struct {
+	Params     map[string]ast.Type
+	ReturnType ast.Type
+}
+
+func FunctionInvocationSignatureFromType(input ast.FunctionType) FunctionInvocationSignature {
+	if input.Params.Kind() != ast.NormalFunctionTypeParamKindIdentifierKind {
+		panic(fmt.Sprintf("Expected normal param kind, found: %d", input.Params.Kind()))
+	}
+
+	fromParams := input.Params.(ast.NormalFunctionTypeParamKindIdentifier).Params
+	params := make(map[string]ast.Type)
+
+	for _, param := range fromParams {
+		params[param.Name.Ident()] = param.Type
+	}
+
+	return FunctionInvocationSignature{
+		Params:     params,
+		ReturnType: input.ReturnType,
+	}
+}
+
 type FunctionInvocation struct {
 	Function string
 	Args     []value.Value
+	// Required so that the VM can automatically pop the function's return value if it materializes into a value.
+	// Furthermore, recursive type assertion is performed on the return value so that the caller does not have to
+	// worry about safety.
+	// Additionally, before the function is actually called, the passed parameter types are also validated.
+	FunctionSignature FunctionInvocationSignature
+}
+
+type FunctionInvocationResult struct {
+	Exception   *VMException
+	ReturnValue value.Value
+}
+
+type VMException struct {
+	CoreNum   uint
+	Interrupt value.VmInterrupt
 }
 
 // Returns the core of the newly spawned process.
 func (self *VM) SpawnAsync(invocation FunctionInvocation, debuggerOut *chan DebugOutput) *Core {
-	// Lookup the function to be invoked.
-	toBeInvoked, found := self.Program.Mappings.Functions[invocation.Function]
-	if !found {
-		panic(fmt.Sprintf("Requested function `%s` does not exist", invocation.Function))
-	}
-
-	return self.spawnCoreInternal(toBeInvoked, invocation.Args, debuggerOut)
+	return self.spawnCoreInternal(invocation.Function, invocation.Args, debuggerOut)
 }
 
 // Spawns a new core but also calls `vm.Wait` internally.
-func (self *VM) SpawnSync(invocation FunctionInvocation, debuggerOut *chan DebugOutput) (coreNum uint, i *value.VmInterrupt) {
-	// Lookup the function to be invoked.
-	toBeInvoked, found := self.Program.Mappings.Functions[invocation.Function]
-	if !found {
-		panic(fmt.Sprintf("Requested function `%s` does not exist", invocation.Function))
+func (self *VM) SpawnSync(invocation FunctionInvocation, debuggerOut *chan DebugOutput) FunctionInvocationResult {
+	if invocation.FunctionSignature.ReturnType == nil {
+		panic("Invocation called without return type specified.")
 	}
 
-	self.spawnCoreInternal(toBeInvoked, invocation.Args, debuggerOut)
-	return self.Wait()
+	// Validate that the provided arguments match the function's signature.
+	expectedParamLen := len(invocation.FunctionSignature.Params)
+	gotArgLen := len(invocation.Args)
+
+	if gotArgLen != expectedParamLen {
+		panic(fmt.Sprintf("Illegal call: expected %d arguments due to function signature, got %d", expectedParamLen, gotArgLen))
+	}
+
+	index := 0
+	for name, param := range invocation.FunctionSignature.Params {
+		arg := invocation.Args[index]
+		_, interrupt := value.DeepCast(arg, param, errors.Span{}, false)
+		if interrupt != nil {
+			panic(fmt.Sprintf("Argument %d for param `%s` type mismatch: %s", index, name, (*interrupt).Message()))
+		}
+
+		index++
+	}
+
+	coreHandle := self.spawnCoreInternal(invocation.Function, invocation.Args, debuggerOut)
+	exceptionCore, interrupt := self.Wait()
+
+	return self.HandleTermination(
+		coreHandle,
+		invocation,
+		interrupt,
+		exceptionCore,
+	)
+}
+
+func (self *VM) HandleTermination(
+	exitCore *Core,
+	invocation FunctionInvocation,
+	interrupt *value.VmInterrupt,
+	exceptionCore uint,
+) FunctionInvocationResult {
+	if interrupt != nil {
+		return FunctionInvocationResult{
+			Exception: &VMException{
+				CoreNum:   exceptionCore,
+				Interrupt: *interrupt,
+			},
+			ReturnValue: nil,
+		}
+	}
+
+	var returnValue value.Value
+
+	switch invocation.FunctionSignature.ReturnType.Kind() {
+	case ast.NullTypeKind, ast.NeverTypeKind, ast.UnknownTypeKind, ast.AnyObjectTypeKind:
+		break
+	default:
+		// Get function return value.
+		returnValueRaw := exitCore.Stack[len(exitCore.Stack)-1]
+
+		// Perform type assertion.
+		castValue, interrupt := value.DeepCast(
+			*returnValueRaw,
+			invocation.FunctionSignature.ReturnType,
+			errors.Span{},
+			false,
+		)
+		if interrupt != nil {
+			panic(fmt.Sprintf("Foreign function invocation: return type assertion failed: %s", (*interrupt).Message()))
+		}
+
+		returnValue = *castValue
+	}
+
+	return FunctionInvocationResult{
+		Exception:   nil,
+		ReturnValue: returnValue,
+	}
 }
 
 // Returns the corenum of the newly spawned process
-func (self *VM) spawnCoreInternal(function string, addToStack []value.Value, debuggerOutput *chan DebugOutput) *Core {
+func (self *VM) spawnCoreInternal(
+	function string,
+	addToStack []value.Value,
+	debuggerOutput *chan DebugOutput,
+) *Core {
+	// Lookup the function to be invoked.
+	toBeInvoked, found := self.Program.Mappings.Functions[function]
+	if !found {
+		panic(fmt.Sprintf("Requested function `%s` does not exist", function))
+	}
+
 	core := self.spawnCore()
 	for _, elem := range addToStack {
 		// TODO: However, the VM should not do this implicitly,
 		// Smarter would be to insert clones manually?
 		core.push(&elem) // Implement a deep copy? Or clone?
 	}
-	go (*core).Run(function, debuggerOutput)
+	go (*core).Run(toBeInvoked, debuggerOutput)
+
 	return core
 }
 
