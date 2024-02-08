591af422ffc5b959a290f95523ff1c9113df1184
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2024-02-08 02:52:22 +0100

    feat: Add type aware JSON unmarshaling

--- homescript/runtime/value/json.go
+++ homescript/runtime/value/json.go
@@ -3,11 +3,33 @@ package value
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"math"
 	"reflect"
+	"strconv"
 
-	"github.com/smarthome-go/homescript/v3/homescript/errors"
+	"github.com/smarthome-go/homescript/v3/homescript/analyzer/ast"
+	herrors "github.com/smarthome-go/homescript/v3/homescript/errors"
 )
+
+// HACK: this is necessary as Go has a really crappy JSON convention.
+// refer to: https://github.com/golang/go/issues/42246
+type jsonFloat float64
+
+func (f jsonFloat) MarshalJSON() ([]byte, error) {
+	const floatSize = 64
+
+	n := float64(f)
+	if math.IsInf(n, 0) || math.IsNaN(n) {
+		return nil, errors.New("unsupported float64")
+	}
+	prec := -1
+	if math.Trunc(n) == n {
+		prec = 1 // Force ".0" for integers.
+	}
+	return strconv.AppendFloat(nil, n, 'f', prec, floatSize), nil
+}
 
 // NOTE: `skipNull` is required so that builtin functions do not show up as `null` in the marshaled output.
 func MarshalValue(self Value, isInner bool) (out interface{}, skipNull bool) {
@@ -17,7 +39,7 @@ func MarshalValue(self Value, isInner bool) (out interface{}, skipNull bool) {
 	case ValueInt:
 		return self.Inner, false
 	case ValueFloat:
-		return self.Inner, false
+		return jsonFloat(self.Inner), false
 	case ValueBool:
 		return self.Inner, false
 	case ValueAnyObject:
@@ -73,8 +95,58 @@ func MarshalValue(self Value, isInner bool) (out interface{}, skipNull bool) {
 	}
 }
 
+func TypeAwareUnmarshalValue(self interface{}, typ ast.Type) *Value {
+	switch self := self.(type) {
+	case string:
+		return NewValueString(self)
+	case float64:
+		if typ.Kind() == ast.IntTypeKind {
+			return NewValueInt(int64(self))
+		}
+		return NewValueFloat(self)
+	case jsonFloat:
+		if typ.Kind() == ast.IntTypeKind {
+			return NewValueInt(int64(self))
+		}
+		return NewValueFloat(float64(self))
+	case int:
+		if typ.Kind() == ast.FloatTypeKind {
+			return NewValueFloat(float64(self))
+		}
+		return NewValueInt(int64(self))
+	case int64:
+		if typ.Kind() == ast.FloatTypeKind {
+			return NewValueFloat(float64(self))
+		}
+		return NewValueInt(self)
+	case bool:
+		return NewValueBool(self)
+	case map[string]interface{}:
+		typeFields := typ.(ast.ObjectType).ObjFields
+
+		fields := make(map[string]*Value)
+		for _, typeField := range typeFields {
+			field := self[typeField.FieldName.Ident()]
+			fields[typeField.FieldName.Ident()] = TypeAwareUnmarshalValue(field, typeField.Type)
+		}
+
+		return NewValueObject(fields)
+	case []interface{}:
+		innerType := typ.(ast.ListType).Inner
+		values := make([]*Value, 0)
+		for _, item := range self {
+			values = append(values, TypeAwareUnmarshalValue(item, innerType))
+		}
+		return NewValueList(values)
+	case nil:
+		return NewNoneOption()
+	default:
+		panic(fmt.Sprintf("Cannot parse unknown JSON value: `%v` (%v) to HMS value", self, reflect.TypeOf(self)))
+	}
+}
+
 // TODO: write docs why this is public
-func UnmarshalValue(span errors.Span, self interface{}) (*Value, *VmInterrupt) {
+func UnmarshalValue(span herrors.Span, self interface{}) (*Value, *VmInterrupt) {
 	// TODO: do this
 	switch self := self.(type) {
 	case string:
@@ -118,7 +190,7 @@ func UnmarshalValue(span errors.Span, self interface{}) (*Value, *VmInterrupt) {
 }
 
 func MarshalToString(self Value) *Value {
-	return NewValueBuiltinFunction(func(executor Executor, cancelCtx *context.Context, span errors.Span, args ...Value) (*Value, *VmInterrupt) {
+	return NewValueBuiltinFunction(func(executor Executor, cancelCtx *context.Context, span herrors.Span, args ...Value) (*Value, *VmInterrupt) {
 		// TODO: fail if skipNull is true?
 		marshaled, _ := MarshalValue(self, false)
 		output, jsonErr := json.Marshal(marshaled)
@@ -130,7 +202,7 @@ func MarshalToString(self Value) *Value {
 }
 
 func MarshalIndentToString(self Value) *Value {
-	return NewValueBuiltinFunction(func(_ Executor, cancelCtx *context.Context, span errors.Span, args ...Value) (*Value, *VmInterrupt) {
+	return NewValueBuiltinFunction(func(_ Executor, cancelCtx *context.Context, span herrors.Span, args ...Value) (*Value, *VmInterrupt) {
 		// TODO: fail if skipNull is true?
 		marshaled, _ := MarshalValue(self, false)
 		output, jsonErr := json.MarshalIndent(marshaled, "", "    ")
