0bd1f542ebe45228008b0f6ecdf8ad724b976035
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2024-02-21 23:21:46 +0100

    fix: Iterate over `zip` files in fuzzing runner

--- cmd/run.sh
+++ cmd/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cmd="go run -v . fuzz validate"
-fuzz_out="fuzz_out"
+# fuzz_out="fuzz_out"
 
-for file in ../examples/*.hms; do
-    in_out_dir="${file//..\/examples/$fuzz_out}"
-    archive_out="${in_out_dir//.hms/.zip}"
-    eval "$cmd $archive_out"
+for archive in fuzz_out/*.zip; do
+    #in_out_dir="${file//..\/examples/$fuzz_out}"
+    #archive_out="${in_out_dir//.hms/.zip}"
+    eval "$cmd $archive"
 done
